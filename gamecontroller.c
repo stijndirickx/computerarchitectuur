10ee9f05db46bd1006ab4e9abd911d5cff8e22bc
Stijn Dirickx <stijn.dirickx@hotmail.com>
2018-02-27 11:21:19 +0100

    oef3

---
+++ gamecontroller.c
@@ -0,0 +1,79 @@
+#define F_CPU 16000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include "Clock.h"
+#include "USART.h"
+#include "Analog.h"
+#include "acc.h"
+#include "LED.h"
+#include "switch.h"
+#include "encoder.h"
+#include "speaker.h"
+
+
+char text[10][10];
+int a;
+
+void SimpleFunction(void);	//A simple function: print a counter (0 to 9) to the terminal
+
+int main(void)
+{
+
+	
+	//###1###
+	
+	//Initialize subsystems
+	ClockInit();			//Initialize system clock (16 MHz)
+	USARTInit();			//Initialize USART and bind to stdout,stdin
+	AnalogInit();			//Initialize ADC
+	AccInit();				//Initialize accelerometer system
+	LEDInit();				//Initialize LEDs
+	SwitchInit();			//Initialize switches
+	EncoderInit();			//Initialize encoder
+	SpeakerInit();			//Initialize speaker system
+	
+	//Enable interrupts
+	PMIC.CTRL|=0b00000111;  //Enable low, medium, high priority interrupts
+	SREG|=0b10000000;		//Globale interrupt enable
+	_delay_ms(500);
+
+	//###2###
+	SimpleFunction();
+	
+	while (1);
+}
+
+void SimpleFunction(void)
+{
+	char text[5][10];
+	
+	int a;
+	for (a=0;a<10;a++)
+	{
+		//The variable text had the same address throughout the execution of the program.
+		sprintf(text[a],"Counter:%d\r",a);
+		puts(text[a]);
+	}
+}
+
+void LightLoop(void)
+{
+	char bitmask = 0b00000001;
+	while (1)
+	{
+		LEDSet(bitmask);
+		if(bitmask == 0b00001000){
+			bitmask >>= 3; //terug eerste led
+		} else {
+			bitmask <<= 1; //bitshift naar volgende led
+		}
+		_delay_ms(500);
+		
+		char gpio_output[5];
+		//Expansion for GPIO input
+		printf(gpio_output, "$SWITCH x\r\n", SwitchGet());
+		puts(gpio_output);
+	};
+}
---
+++ switch.c
@@ -0,0 +1,28 @@
+#include "switch.h"
+#include <avr/io.h>
+
+void SwitchInit(void)
+{
+	//11,13,15 p124
+	
+	PORTD.DIRCLR |= 0b11110001;
+	PORTD.PIN0CTRL= 0b00000001;		//indrukkenn
+	PORTD.PIN4CTRL= 0b00000001;		//rechts
+	PORTD.PIN5CTRL= 0b00000001;		//beneden
+	PORTD.PIN6CTRL= 0b00000001;		//links
+	PORTD.PIN7CTRL= 0b00000001;		//omhoog
+}
+
+
+char SwitchGet(void)
+{
+	PORTB.PIN0CTRL = PORTD.PIN0CTRL;
+	PORTB.PIN1CTRL = PORTD.PIN4CTRL;
+	PORTB.PIN2CTRL = PORTD.PIN5CTRL;
+	PORTB.PIN3CTRL = PORTD.PIN6CTRL;
+	PORTB.PIN4CTRL = PORTD.PIN7CTRL;
+	return PORTB.IN;
+}
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
