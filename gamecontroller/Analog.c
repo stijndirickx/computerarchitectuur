082df9b1f5f1c8934fa4d5a095b62a228fb66236
rubends <rubendeswaef@gmail.com>
2018-03-26 15:59:03 +0200

    update

--- gamecontroller/Analog.c
+++ gamecontroller/Analog.c
@@ -3,6 +3,8 @@
 
 void AnalogInit(void)
 {
+	ADCA.REFCTRL = 0b0000010; // 0 - 000 (INT1V: referentie 1V) - 0 - 1 (bandgap enable: 'verboden zone') - 0 (temp sensor uit)
+	ADCA.PRESCALER = 0b00000010; // 010 = DIV16 = divide by 16 --> 16 MHz / 16 = 1 MHz
 
 }
 int AnalogGetCh(int PinPos,int PinNeg)
