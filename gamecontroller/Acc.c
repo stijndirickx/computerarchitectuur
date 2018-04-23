886e158584b936a9d201334f0057469312921bef
Stijn Dirickx <stijn.dirickx@hotmail.com>
2018-04-23 17:16:22 +0200

    acc init

--- gamecontroller/Acc.c
+++ gamecontroller/Acc.c
@@ -5,10 +5,16 @@
 
 void AccInit(void)
 {
+	//De accelerometer dient geïnitialiseerd te worden.
+	//Dit houdt in dat de accelerometer uit slaaptoestand moet gehaald worden. 
+
 	//accelerometer uit slaaptoestand
 	// PC6 = ACC_SLEEP
+	PORTA.DIRSET = 0b01000000;
 	PORTA.PIN6CTRL = 0b00010001; // 0 reserved - 0 inverted IO enable - 010 Pull-down (on input) - 001 Sense rising edge
 
+	//accelerometer actief zetten
+	PORTA.OUTSET = 0b01000000; //driven high
 	
 }
 unsigned int AccGetXAxisRaw(void)
